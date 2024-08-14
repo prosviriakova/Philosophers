cba5e32f0062178b3cf23147eee9954cb73d38aa
oprosvir <ol7897@ya.ru>
2024-08-15 01:49:37 +0200

    lib included

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,11 +6,18 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 15:51:24 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/08/12 15:53:11 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/08/15 01:47:53 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+#include <stdio.h>      // printf
+#include <stdlib.h>     // malloc, free
+#include <string.h>     // memset
+#include <unistd.h>     // usleep, write
+#include <sys/time.h>   // gettimeofday
+#include <pthread.h>    // pthread_xxx
 
 #endif
