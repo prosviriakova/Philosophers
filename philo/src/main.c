1beb80e2cd218ab8a971da76ba5d58c1bcf44a68
oprosvir <ol7897@ya.ru>
2024-08-17 15:20:27 +0200

    add: validate input/arguments (positive digits > 0), atoi without sign

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,50 +6,68 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 15:50:01 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/08/16 23:48:43 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/08/17 15:09:10 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int parse_arguments(char **argv, int *num_philosophers, t_philo *philos)
+static int	parse_arguments(char **argv, int *num_philosophers, t_philo *philos)
 {
-    int i;
+	int	i;
 
-    *num_philosophers = ft_atoi(argv[1]);
-
-    for (i = 0; i < *num_philosophers; i++)
-    {
-        philos[i].id = i + 1;
-        philos[i].time_to_die = ft_atoi(argv[2]);
-        philos[i].time_to_eat = ft_atoi(argv[3]);
-        philos[i].time_to_sleep = ft_atoi(argv[4]);
-    }
-    
-    return (0);
+	*num_philosophers = ft_atoi(argv[1]);
+	for (i = 0; i < *num_philosophers; i++)
+	{
+		philos[i].id = i + 1;
+		philos[i].time_to_die = ft_atoi(argv[2]);
+		philos[i].time_to_eat = ft_atoi(argv[3]);
+		philos[i].time_to_sleep = ft_atoi(argv[4]);
+	}
+	return (0);
 }
 
-int main(int argc, char *argv[])
+static void	validate_input(int argc, char **argv)
 {
-    t_philo *philos;
-    int num_philosophers;
+	int	i;
+	int	j;
+	int	value;
 
-    if (argc < 5 || argc > 6)
-        error_exit(HELP_MSG);
+	if (argc < 5 || argc > 6)
+		error_exit(HELP_MSG);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				error_exit("Arguments must be positive integers.");
+			j++;
+		}
+		value = ft_atoi(argv[i]);
+		if (value <= 0)
+			error_exit("Invalid argument: must be > 0.");
+		i++;
+	}
+}
 
-    num_philosophers = ft_atoi(argv[1]);
-    philos = (t_philo *)malloc(sizeof(t_philo) * num_philosophers);
-    if (!philos)
-        error_exit("Memory allocation failed!");
+int	main(int argc, char *argv[])
+{
+	t_philo	*philos;
+	int		num_philosophers;
 
-    if (parse_arguments(argv, &num_philosophers, philos))
-    {
-        free(philos);
-        return (1);
-    }
-
-    start_simulation(num_philosophers, philos);
-
-    free(philos);
-    return (0);
+	validate_input(argc, argv);
+	num_philosophers = ft_atoi(argv[1]);
+	philos = (t_philo *)malloc(sizeof(t_philo) * num_philosophers);
+	if (!philos)
+		error_exit("Memory allocation failed!");
+	if (parse_arguments(argv, &num_philosophers, philos))
+	{
+		free(philos);
+		return (1);
+	}
+	start_simulation(num_philosophers, philos);
+	free(philos);
+	return (0);
 }
--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 21:39:44 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/08/16 23:01:40 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/08/17 14:55:04 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,16 @@
 int	ft_atoi(const char *str)
 {
 	long res;
-	long sign;
 
 	res = 0;
-	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
 		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -1;
-		str++;
-	}
 	while (*str >= '0' && *str <= '9')
 	{
 		res = res * 10 + (*str - '0');
-		if (sign > 0 && res < 0)
-			return (-1);
-		if (sign < 0 && res < 0)
-			return (0);
 		str++;
 	}
-	return ((int)(res * sign));
+	return ((int)res);
 }
 
 void	error_exit(char *msg)
