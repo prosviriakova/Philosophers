f1e7a8e32c495e601825fc693705c684f65cbec1
oprosvir <ol7897@ya.ru>
2024-08-17 16:36:38 +0200

    quick norm fix

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 15:50:01 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/08/17 16:27:10 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/08/17 16:34:38 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,50 +37,49 @@ static void	validate_input(int argc, char **argv)
 	}
 }
 
-static void init_philosophers(t_data *data)
+static void	init_philosophers(t_data *data)
 {
-	int i;
+	int	i;
 
 	i = 0;
-    data->philos = malloc(sizeof(t_philo) * data->num_philosophers);
-    if (!data->philos)
-    {
-        error_exit("Error: Memory allocation failed.");
-    }
+	data->philos = malloc(sizeof(t_philo) * data->num_philosophers);
+	if (!data->philos)
+	{
+		error_exit("Error: Memory allocation failed.");
+	}
 	while (i < data->num_philosophers)
 	{
 		data->philos[i].id = i + 1;
-        i++;
+		i++;
 	}
 }
 
-static void init_data(t_data *data, char **argv)
+static void	init_data(t_data *data, char **argv)
 {
-    data->num_philosophers = ft_atoi(argv[1]);
-    data->time_to_die = ft_atoi(argv[2]);
-    data->time_to_eat = ft_atoi(argv[3]);
-    data->time_to_sleep = ft_atoi(argv[4]);
-    if (argv[5])
+	data->num_philosophers = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5])
 		data->must_eat_count = ft_atoi(argv[5]);
 	else
 		data->must_eat_count = -1;
-    init_philosophers(data);
+	init_philosophers(data);
 }
 
-
-void clean_up(t_data *data)
+void	clean_up(t_data *data)
 {
-    // Free all allocated memory, destroy mutexes, etc.
-    free(data->philos);
+	// Free all allocated memory, destroy mutexes, etc.
+	free(data->philos);
 }
 
 int	main(int argc, char **argv)
 {
-    t_data  data;
+	t_data	data;
 
 	validate_input(argc, argv);
-    init_data(&data, argv);
+	init_data(&data, argv);
 	start_simulation(&data);
-    clean_up(&data);
+	clean_up(&data);
 	return (0);
 }
--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 21:39:44 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/08/17 14:55:04 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/08/17 16:35:48 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_atoi(const char *str)
 {
-	long res;
+	long	res;
 
 	res = 0;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
