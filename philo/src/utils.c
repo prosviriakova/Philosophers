821ac2a4540d270e993ed489122208e2bbb9906c
oprosvir <ol7897@ya.ru>
2024-10-08 09:16:10 +0200

    utils simplif

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 21:39:44 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/08/17 16:35:48 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/08/23 11:30:50 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_atoi(const char *str)
 	long	res;
 
 	res = 0;
-	while ((*str >= 9 && *str <= 13) || *str == 32)
-		str++;
 	while (*str >= '0' && *str <= '9')
 	{
 		res = res * 10 + (*str - '0');
