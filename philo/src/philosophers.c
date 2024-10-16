d740da45153e11654aa938497728335b41787f54
oprosvir <ol7897@ya.ru>
2024-10-16 18:45:41 +0200

    working on routine, need to add: death cheking, monitoring thread

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 01:17:15 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/10/16 13:27:41 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:32:25 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,35 @@ static void	*single(t_philo *philosopher, t_data *data)
 	return (NULL);
 }
 
+void	take_forks(t_philo *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		print_status(philo, "has taken a fork");
+		pthread_mutex_lock(philo->left_fork);
+		print_status(philo, "has taken a fork");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->left_fork);
+		print_status(philo, "has taken a fork");
+		pthread_mutex_lock(philo->right_fork);
+		print_status(philo, "has taken a fork");
+	}
+}
+
+void	eat(t_philo *philo)
+{
+	t_data	*data;
+
+	data = philo->data;
+	philo->meals_eaten++;
+	philo->last_meal_time = get_timestamp_in_ms() - data->start_time;
+	print_status(philo, "is eating");
+	usleep(data->time_to_eat * 1000);
+}
+
 void	*philosopher_routine(void *arg)
 {
 	t_philo	*philosopher;
@@ -31,12 +60,16 @@ void	*philosopher_routine(void *arg)
 	data = philosopher->data;
 	if (data->num_philosophers == 1)
 		return (single(philosopher, data));
-	print_status(philosopher, "is thinking");
-	usleep(2000);
-	print_status(philosopher, "is eating");
-	usleep(data->time_to_eat * 1000);
-	print_status(philosopher, "is sleeping");
-	usleep(data->time_to_sleep * 1000);
+	while (data->must_eat_count == -1 || philosopher->meals_eaten < data->must_eat_count)
+	{
+		take_forks(philosopher);
+		eat(philosopher);
+		pthread_mutex_unlock(philosopher->left_fork);
+		pthread_mutex_unlock(philosopher->right_fork);
+		print_status(philosopher, "is sleeping");
+		usleep(data->time_to_sleep * 1000);
+		print_status(philosopher, "is thinking");
+	}
 	return (NULL);
 }
 
@@ -48,7 +81,9 @@ int	start_simulation(t_data *data)
 	data->start_time = get_timestamp_in_ms();
 	while (i < data->num_philosophers)
 	{
-		if (pthread_create(&data->philos[i].thread, NULL, philosopher_routine, &data->philos[i]) != 0)
+		data->philos[i].last_meal_time = data->start_time;
+		if (pthread_create(&data->philos[i].thread, NULL, philosopher_routine,
+				&data->philos[i]) != 0)
 			return (1);
 		i++;
 	}
